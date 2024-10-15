e5b704d795efc6b0ddf4e8ab038394209443986a
NedzunganiK <nedzunganik8@gmail.com>
2024-10-15 10:04:19 -0700

    Async Comprehension

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-'''Task 5's module.
-'''
+
+"""A type-annotated function `sum_list` which takes a list `input_list` of
+floats as argument and returns their sum as a float."""
+
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''Computes the sum of a list of floating-point numbers.
-    '''
-    return float(sum(input_list))
+    """Takes a list `input_list` of
+    floats as argument and returns their sum as a float.
+    """
+    return sum(input_list)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,3 @@
+0x02. Python - Async Comprehension
+
+We propose to allow using async for inside list, set and dict comprehensions. Pending PEP 525 approval, we can also allow creation of asynchronous generator expressions.
